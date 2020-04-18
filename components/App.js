3c18a2a585185f469cc9e4ab6d076380a0dd3a30
Sara Blichowska <51542737+SaraBlich@users.noreply.github.com>
2020-04-18 23:07:03 +0200

    Updated - added setvalue, value and selectedindex

--- components/App.js
+++ components/App.js
@@ -1,29 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { ThemeProvider } from '@material-ui/core/styles';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import theme from '../components/UI/Theme';
 import Header from '../components/UI/Header';
+import Footer from '../components/UI/Footer';
+
 
 function App() {
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const [value, setValue] = useState(0);
   return (
     <ThemeProvider theme = {theme}> 
      
      <BrowserRouter>
-      <Header /> 
+      <Header  value = {value} setValue = {setValue} selectedIndex = {selectedIndex} setSelectedIndex = {setSelectedIndex}/> 
         <Switch>
-          <Route exact path = "/" component = {()=> <div>home</div>}/>
-          <Route exact path = "/services" component = {()=> <div>services</div>}/>
-          <Route exact path = "/custom software" component = {()=> <div>custom software</div>}/>
-          <Route exact path = "/mobileapps" component = {()=> <div>mobileapps</div>}/>
-          <Route exact path = "/websites" component = {()=> <div>websites</div>}/>
-          <Route exact path = "/revolution" component = {()=> <div>revolution</div>}/>
-          <Route exact path = "/about" component = {()=> <div>about</div>}/>
-          <Route exact path = "/contact" component = {()=> <div>contact</div>}/>
-          <Route exact path = "/estimate" component = {()=> <div>estimate</div>}/>
-          {/* <Route exact path = "/customsoftware" component = {() => <div>Custom Software</div>}/>
-          <Route exact path = "/mobileapps" component = {() => <div>Mobile Apps</div>}/>
-          <Route exact path = "/websites" component = {() => <div>Websites</div>}/> */}
+          <Route exact path = "/" component = {()=> <div style = {{height: "2000px"}}>home</div>}/>
+          <Route exact path = "/services" component = {()=> <div style = {{height: "2000px"}}>services</div>}/>
+          <Route exact path = "/customsoftware" component = {()=> <div style = {{height: "2000px"}}>custom software</div>}/>
+          <Route exact path = "/mobileapps" component = {()=> <div style = {{height: "2000px"}}>mobileapps</div>}/>
+          <Route exact path = "/websites" component = {()=> <div style = {{height: "2000px"}}>websites</div>}/>
+          <Route exact path = "/revolution" component = {()=> <div style = {{height: "2000px"}}>revolution</div>}/>
+          <Route exact path = "/about" component = {()=> <div style = {{height: "2000px"}}>about</div>}/>
+          <Route exact path = "/contact" component = {()=> <div style = {{height: "2000px"}}>contact</div>}/>
+          <Route exact path = "/estimate" component = {()=> <div style = {{height: "2000px"}}>estimate</div>}/>
+          
           </Switch>
+     
+     <Footer value = {value} setValue = {setValue} selectedIndex = {selectedIndex} setSelectedIndex = {setSelectedIndex}/>
      </BrowserRouter>
     </ThemeProvider>
   );
