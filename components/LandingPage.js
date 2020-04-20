c4b23cb3bdb5c94e3683adbac88f36f3a47e266e
Sara Blichowska <51542737+SaraBlich@users.noreply.github.com>
2020-04-20 17:25:08 +0200

    Updated LandingPage.js

--- components/LandingPage.js
+++ components/LandingPage.js
@@ -14,6 +14,7 @@ import customSoftwareIcon from '../assets/Custom Software Icon.svg';
 import mobileAppsIcon from '../assets/mobileIcon.svg';
 import websitesIcon from '../assets/websiteIcon.svg';
 import revolutionBackground from '../assets/repeatingBackground.svg';
+import infoBackground from '../assets/infoBackground.svg';
 
 const useStyles = makeStyles(theme => ({
     animation:
@@ -56,6 +57,7 @@ const useStyles = makeStyles(theme => ({
     {
         ...theme.typography.learnButton,
         fontSize: "0.7rem",
+        
         height: 35,
         padding: 5,
         [theme.breakpoints.down("sm")]:
@@ -134,6 +136,16 @@ const useStyles = makeStyles(theme => ({
             borderRadius: 0,
             width: "100%"
         }
+    },
+    
+    infoBackground:
+    {
+        backgroundImage: `url(${infoBackground})`,
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        height: "100%",
+        width: "100%"
     }
 
 }));
@@ -143,6 +155,7 @@ export default function LandingPage(){
     const classes = useStyles();
     const theme = useTheme();
     const matchesSM = useMediaQuery(theme.breakpoints.down("sm"));
+    const matchesXS = useMediaQuery(theme.breakpoints.down("xs"));
     
     const defaultOptions = {
         loop: true,
@@ -250,7 +263,7 @@ export default function LandingPage(){
                 </Grid>
                 
             </Grid>
-         {/* -------------------Revolution card-------------------- */}
+             {/* -------------------Revolution card-------------------- */}
             <Grid item>
                 <Grid container style = {{height: "100em", marginTop: "10em"}} alignItems = "center" justify = "center">
                 <Card className = {classes.revolutionCard}>
@@ -276,6 +289,49 @@ export default function LandingPage(){
                 </Card>
                 <div className = {classes.revolutionBackground} />
             </Grid>
+            {/* ------------------Information block-------------------- */}
+            <Grid container direction = "row" style = {{height: "80em" }} alignItems = "center">
+                <Grid item container style = {{position: "absolute", textAlign: matchesXS ? "center" : "inherit"}} direction = {matchesXS ? "column" : "row"}>
+                <Grid item sm style = {{marginLeft: matchesSM ? 0 : matchesSM ? "2em" : "5em"}}>
+                    <Grid container direction = "column" >
+                        <Typography variant = "h2" style = {{color: "white"}}>
+                            About Us
+                        </Typography>
+                       
+                        <Typography variant = "subtitle2" gutterBottom>
+                           Let's get personal.
+                        </Typography>
+                        <Grid item>
+                            <Button variant = "outlined" className = {classes.learnButton} style = {{color: "white", borderColor: "white"}}>
+                                        <span style = {{marginRight: 10}}> Learn More </span>
+                                    <ButtonArrow width = {15} height = {15} fill = "white"/>
+                            </Button>
+                        </Grid>
+                    </Grid>
+
+                </Grid>
+                <Grid item sm style = {{marginRight: matchesXS ? 0 : matchesSM ? "2em" : "5em", textAlign: matchesXS ? "center" : "right"}}>
+                    <Grid container direction = "column" >
+                        <Typography variant = "h2" style = {{color: "white"}}>
+                            Contact Us
+                        </Typography>
+                       
+                        <Typography variant = "subtitle2" gutterBottom>
+                        Say hello! <span role = "img" aria-label = "waving hand">👋</span>
+                        </Typography>
+                        <Grid item>
+                            <Button variant = "outlined" className = {classes.learnButton} style = {{color: "white", borderColor: "white"}}>
+                                        <span style = {{marginRight: 10}}> Learn More </span>
+                                    <ButtonArrow width = {15} height = {15} fill = "white"/>
+                            </Button>
+                        </Grid>
+                    </Grid>
+
+                </Grid>
+                </Grid>
+                <div className = {classes.infoBackground}/>
+            </Grid>
+
             </Grid>
             
         </Grid>
@@ -284,3 +340,7 @@ export default function LandingPage(){
 }
 
 //gutterBottom for the gap beneath 'the revolution' h3
+// style = {{height : "100em"}} is mandatory, as we want our image display properly
+// <Grid item style = {{position: "absolute"}}> needs to be added, as the text should be on the top of the image
+
+
